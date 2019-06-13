44e5bd6909b7b0e6583472ca2610fcf87c38ef93
Xurxe Toivo García <xurxe.garcia@integrify.io>
2019-06-13 14:06:34 +0300

    Develop (#1)
    * Fixing build bug
    
    * Fixing build bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: `Integrify Design Bootcamp`,
-    description: `Prep project for Integrify Design Bootcamp (June 26th-28th, 2019)`,
-    author: `André Vollrath and Xurxe Toivo García`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Integrify Design Bootcamp`,
+        description: `Prep project for Integrify Design Bootcamp (June 26th-28th, 2019)`,
+        author: `André Vollrath and Xurxe Toivo García`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-manifest`
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-manifest`
+    ],
 }
